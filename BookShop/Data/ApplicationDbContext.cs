24da6d44cf4a3e2bfcd5c73fe714d7e5af6be4f2
DerickCJ <xiaoshenqian@gmail.com>
2024-10-13 13:31:19 -0400

    添加项目文件。

---
+++ BookShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using BookShop.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace BookShop.Data
+{
+    
+        public class ApplicationDbContext : IdentityDbContext
+
+        {
+
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+
+           : base(options)
+
+            {
+
+            }
+
+            public DbSet<Book> Books { get; set; }
+
+            public DbSet<Category> Categories { get; set; }
+
+            public DbSet<CartItem> CartItems { get; set; }
+
+            public DbSet<Order> Orders { get; set; }
+
+            public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        }
+    
+}
